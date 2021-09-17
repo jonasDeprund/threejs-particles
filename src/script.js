3e98fa0f8d25bb4949b2fac46bd1eb32247c6545
Jonas Deprund <jonasdeprund@gmail.com>
2021-09-17 15:57:12 +0200

    particles project done

--- src/script.js
+++ src/script.js
@@ -31,15 +31,19 @@ const count = 500;
 // Images random
 
 const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
 
 for (let i = 0; i < count * 3; i++) {
   positions[i] = (Math.random() - 0.5) * 10;
+  colors[i] = Math.random();
 }
 
 particlesGeometry.setAttribute(
   'position',
   new THREE.BufferAttribute(positions, 3)
 );
+
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
 
 // Material
 const particlesMaterial = new THREE.PointsMaterial();
@@ -52,6 +56,7 @@ particlesMaterial.alphaMap = particleTexture;
 // particlesMaterial.depthTest = false;
 particlesMaterial.depthWrite = false;
 particlesMaterial.blending = THREE.AdditiveBlending;
+particlesMaterial.vertexColors = true;
 
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
@@ -121,6 +126,20 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // update particles
+  // particles.rotation.y = elapsedTime * 0.2;
+
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3;
+
+    const x = particlesGeometry.attributes.position.array[i3 + 0];
+    particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(
+      elapsedTime + x
+    );
+  }
+
+  particlesGeometry.attributes.position.needsUpdate = true;
 
   // Update controls
   controls.update();
